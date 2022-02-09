f857359b05110646ccfc488d8dda91ed59a54afb
GaryCUP <47047840+GaryCUP@users.noreply.github.com>
2022-02-09 14:31:14 -0500

    will modify cycle to read location

--- cycle.js
+++ cycle.js
@@ -2,11 +2,13 @@
  * Day/Night Gradient Cycle
  */
 
+//const SunCalc = require("../js/suncalc");
+
 // Get the current hour; JS runs on the 24-hour clock
 var hour = new Date().getHours();
 
 // Assign the element #sky to an easy-to-use variable
-var sky  = document.querySelector("#sky");
+var sky  = document.querySelector("#body");
 
 /**
  * Now we'll create objects for each block of time, and store
@@ -15,10 +17,13 @@ var sky  = document.querySelector("#sky");
  * how to wrap around from 21:00 to 5:00. This could be accounted
  * for with some math, but I don't feel like it.
  */
+
+//note:eventually, make times of sun position based on user location
 var timeBlocks = [
   { // Night starts at 9pm/21:00 and ends at 11pm/24:00
     start: 21,
-    end:   24,
+    //start:SunCalc.getTimes(new Date(),0,0) ,
+    end:   23,
     class: "night",
   },
   { // Night starts again at 12am/00:00 and ends at 5am/5:00
@@ -56,5 +61,6 @@ for ( var i = 0; i < timeBlocks.length; i++ ) { /* 1 */
   
   if ( timeOfDay.start <= hour && hour <= timeOfDay.end ) { /* 3 */
     sky.addClass(timeOfDay.class); /* 4 */
+    //sky.classList.add("timeOfDay.class");
   }
 }
